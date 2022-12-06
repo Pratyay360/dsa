794215d477b82906697fc2ef68eb9dda8d7949b6
Pratyay360 <pratyaymustafi@gmail.com>
2022-12-06 22:46:48 +0530

    dsa1

--- ds/linkedlist.c
+++ ds/linkedlist.c
@@ -1,5 +1,86 @@
 #include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+}*head,*temp,*temp1,*temp2;
+void insert_list()
+{
+    int item;
+    printf("Enter a item : ");
+    scanf("%d", &item);
+    if(head==NULL)
+    {
+        head = (struct node*)malloc(sizeof(struct node));
+        head->data = item;
+        head->next = NULL;
+    }
+    else
+    {
+        struct node *temp1 = (struct node*)malloc(sizeof(struct node));
+        temp1->data = item;
+        temp1->next = head;
+        head=temp1;
+        
+    }
+}
+void delete_list()
+{
+    if(head==NULL)
+    {
+        printf("List is empty");
+    }
+    else
+    {
+        temp2 = head;
+        head = head->next;
+        temp2->next = NULL;
+        printf("DELETED : %d", temp2->data);
+        free(temp2);
+    }
+}
+void display()
+{
+    if(head==NULL)
+    {
+        printf("List is empty");
+    }
+    else
+    {
+        temp2 = head;
+        while(temp2!=NULL)
+        {
+            printf("[%d]-> ", temp2->data);
+            temp2 = temp2->next;
+        }
+    }
+}
 int main()
 {
-    
+    int n;
+    while(1)
+    {
+        printf("\n1. Insert | 2. Delete | 3. Display | 4. Exit");
+        printf("\nEnter a option : ");
+        scanf("%d", &n);
+        switch (n)
+        {
+        case 1:
+            insert_list();
+            break;
+        case 2:
+            delete_list();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+            break;
+        default:
+            continue;
+        }
+    }
+    return 0;
 }
--- ds/stack.c
+++ ds/stack.c
@@ -1,5 +1,77 @@
 #include<stdio.h>
+#include<stdlib.h>
+#define max 5
+int stack[max];
+int top = -1;
+void push()
+{
+    if(top==max-1)
+    {
+        printf("Stack is full");
+    }
+    else
+    {
+        int item;
+        printf("Enter a item : ");
+        scanf("%d", &item);
+        top++;
+        stack[top] = item;
+    }
+}
+void pop()
+{
+    if(top==-1)
+    {
+        printf("Stack is empty");
+    }
+    else
+    {
+        printf("POP : %d", stack[top]);
+        top--;
+    }
+
+}
+void display()
+{
+    if(top==-1)
+    {
+        printf("Stack is empty");
+    }
+    else
+    {
+        int i=top;
+        while(i>=0)
+        {
+            printf("[%d]\n", stack[i]);
+            i--;
+        }
+    }
+}
 int main()
 {
-    
+    int n;
+    while(1)
+    {
+        printf("\n1. Push | 2. Pop | 3. Display | 4. Exit");
+        printf("\nEnter a option : ");
+        scanf("%d", &n);
+        switch (n)
+        {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+            break;
+        default:
+            continue;
+        }
+    }
+    return 0;
 }
